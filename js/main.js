b8000990b6ae8845b7b765f95823929209bdc80d
Martina Rodriguez Arona <tini.arona16@gmail.com>
2024-09-20 20:50:04 -1200

    agregamos un nav

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+//alert("Hola desde main.js")
+console.log("hola desde main.js")
+console.error("error")
+
+let X = 10
+const Y = 5
+
+let nombre = "Ramiro"
+
+X = 15
+
+console.log(X,Y)
+console.log(X+Y)
+console.log("Tu nombre es: " + nombre) //Concatenar
+console.log(`Tu nombre es: ${nombre} y tu edad es: ${X} `) //Concatenar
+
+const NU = prompt('ingrese su nombre: ') //es donde el usuario puede escribir lo que quiera
+
+console.log(NU)
